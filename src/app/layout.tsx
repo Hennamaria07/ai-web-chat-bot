aba05541a2350eae6a1f440fbb8e95777f9ff97d
Henna Maria <hennamaria2001@gmail.com>
2024-09-22 15:35:56 +0530

    layout wrapped with provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Provider from "@/components/Provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
-        {children}
+       <Provider>
+       <main className="h-screen dark text-foreground bg-background">
+          {children}
+        </main>
+       </Provider>
       </body>
     </html>
   );
